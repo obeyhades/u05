707137643f6f6b97cf2509d63a7f7d83cef8dd33
obeyhades <abdu.nazar145@gmail.com>
2025-03-12 19:17:32 +0100

    added routes for users

--- build_TS/src/routes/userRoute.ts
+++ build_TS/src/routes/userRoute.ts
@@ -1,10 +1,18 @@
 import express, {Request, Response } from "express";
 import { userModel } from "../models/userModels"
-import { createUser, deleteUser } from "../controllers/userControllers";
+import { createUser, deleteUser, getUsers, getUserById,updateUser } from "../controllers/userControllers";
+
 
 const router = express.Router()
 
 router.post("/", createUser)
+router.get("/", getUsers)
+router.get("/")
 router.delete("/", deleteUser)
+
+router.get('/:id', getUserById)
+router.put("/:id", updateUser)
+
+
 
 export { router as userRouter }
